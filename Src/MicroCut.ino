3aecae009f5640d2f72d4fed6aa7b566b436f563
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2016-11-06 21:10:45 +0100

    Arduino Firmware

---
+++ Src/MicroCut.ino
@@ -0,0 +1,40 @@
+/*
+ * MicroCut.ino : power supply interruption generator
+ * autor : JPP
+ * V1.00 dated 06/11/2016
+ */
+
+int buttonPin = 13; // "Cut" button
+int relayPin = A0;
+int potyPin = A1;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // waits for "Cut" button release
+  while(digitalRead(buttonPin) == LOW);
+  
+  // waits for "Cut" button press
+  while(digitalRead(buttonPin) == HIGH);
+  
+  // reads poty position
+  int potyPos = analogRead(potyPin);
+  
+  // scales [0..1023] to [20000..50000] (20 to 50ms)
+  long int microSeconds = 29.3255 * potyPos + 20000.5;
+  
+  // pulse the relay for the required duration
+  digitalWrite(relayPin, HIGH);
+  delayMicroseconds(microSeconds - 5000); // removes 5ms for relay delay
+  digitalWrite(relayPin, LOW);
+  
+  // trace message to serial port
+  Serial.print(0.001 * microSeconds);
+  Serial.println(" ms");
+  
+  delay(200); // debouncing
+}
